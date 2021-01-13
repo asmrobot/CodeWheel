47f03746d349062e5174157fe9f2127c7b643dcb
asmrobot <asmrobot@hotmail.com>
2021-01-13 18:23:20 +0800

    add query first default

--- Templates/CodeWheel.Templates.DBService/RealTemplate.cs
+++ Templates/CodeWheel.Templates.DBService/RealTemplate.cs
@@ -57,24 +57,121 @@ namespace CodeWheel.Templates.DBService
             }
         }
 
+        private const string Generate_Dir = "Generates";
         public override  bool CreateFiles(ref string msg, string saveDir, List<TableMeta> tables, GenerateFileDelegate generateFileFunc, UIVOBase vo)
         {
             msg = string.Empty;
             UIVO viewModel = vo as UIVO;
 
+            //生成基类
+            GenerateBaseClassFile(saveDir, viewModel);
+
             for (int i = 0; i < tables.Count; i++)
             {
                 viewModel.CurrentTable = tables[i];
-                string file = Path.Combine(saveDir, viewModel.CurrentTable.UpperCamelName + viewModel.ClassFix + ".cs");
+                string file = Path.Combine(saveDir, Generate_Dir, viewModel.CurrentTable.UpperCamelName + viewModel.ClassFix + ".cs");
 
                 if (!generateFileFunc(file, TEMPLATE_NAME, typeof(UIVO), viewModel))
                 {
                     continue;
                 }
+
+                GeneratePartialClassFile(saveDir, viewModel);
             }
 
             return true;
         }
 
+        /// <summary>
+        /// 生成部分类
+        /// </summary>
+        /// <param name="saveDir"></param>
+        /// <param name="viewModel"></param>
+        private void GeneratePartialClassFile(string saveDir, UIVO viewModel)
+        {
+            const string template = @"$import_namespace$
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Collections.Generic;
+using ZTImage;
+using System.Threading.Tasks;
+using Dapper;
+using ZTImage.DbLite;
+
+
+namespace Zhuomi.Business
+{
+    public partial class $classname$
+    {
+        
+        public $classname$(DbConnectionFactory connectionFactory):base(connectionFactory)
+        {
+            
+        }
+    }
+}";
+            string dir = Path.Combine(saveDir, viewModel.CurrentTable.UpperCamelName + viewModel.ClassFix + ".cs");
+            File.WriteAllText(dir, template
+                .Replace("$import_namespace$", viewModel.ImportNameSpace)
+                .Replace("$classname$", viewModel.CurrentTable.UpperCamelName + viewModel.ClassFix)
+                );
+
+        }
+
+        /// <summary>
+        /// 生成基类文件
+        /// </summary>
+        /// <param name="saveDir"></param>
+        /// <param name="viewModel"></param>
+        private void GenerateBaseClassFile(string saveDir,UIVO viewModel)
+        {
+            var path = Path.Combine(saveDir, Generate_Dir);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            const string baseClassText = @"using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using ZTImage.DbLite;
+
+namespace $namespace$
+{
+    public class ServiceBase
+    {
+        public ServiceBase(DbConnectionFactory factory)
+        {
+            this.conFactory = factory;
+        }
+
+        protected DbConnectionFactory conFactory;
+        protected IDbConnection ConnectionWrap(IDbTransaction transaction, out bool needClose)
+        {
+            IDbConnection connection = null;
+            if (transaction != null)
+            {
+                needClose = false;
+                connection = transaction.Connection;
+            }
+            else
+            {
+                needClose = true;
+                connection = conFactory.CreateConnection();
+            }
+
+            return connection;
+        }
+    }
+}
+";
+            string dir = Path.Combine(saveDir, Generate_Dir,"ServiceBase.cs");
+
+            File.WriteAllText(dir, baseClassText
+                .Replace("$namespace$", viewModel.NameSpace)
+                );
+        }
     }
 }
